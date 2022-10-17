382de73f0f82656057881825fa2e61c408217468
helen121027 <helen121027@gmail.com>
2022-10-18 01:05:49 +0900

    221017 노마드코더 RN(기초) Weather API

--- React Native/Weather/App.js
+++ React Native/Weather/App.js
@@ -1,19 +1,49 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useEffect, useState } from 'react';
+import { StyleSheet, Text, View, ScrollView, Dimensions } from 'react-native';
+
+import * as Location from 'expo-location';
+
+const { width: SCREEN_WIDTH } = Dimensions.get('window');
+
+const API_KEY = "6a596e75a951b56f7a2582db316d341d";
 
 export default function App() {
+  const [city, setCity] = useState("Loading...")
+  const [days, setDays] = useState([]);
+  const [ok, setOk] = useState(true);
+  const getWeather = async() => {
+    const {granted} = await Location.requestForegroundPermissionsAsync();
+    // console.log(permission);
+    if (!granted){
+      setOk(false);
+    }
+    const {coords:{latitude, longitude}} = await Location.getCurrentPositionAsync({accuracy: 5});
+    const location = await Location.reverseGeocodeAsync({latitude, longitude}, {useGoogleMaps: false});
+
+    setCity(location[0].district);
+    const response = await fetch(`https://pro.openweathermap.org/data/2.5/forecast/hourly?lat=${latitude}&lon=${longitude}&appid=${API_KEY}`);
+    const json = await response.json();
+    setDays(json.daily);
+    console.log(json);
+  }
+  useEffect(() => {
+    getWeather();
+  }, []);
+
   return (
     <View style={styles.container}>
       <StatusBar />
       <View style={styles.city}>
-        <Text style={styles.cityName}>Seoul</Text>
+        <Text style={styles.cityName}>{city}</Text>
       </View>
-      <View style={styles.weather}>
-        <View style={styles.day}>
-          <Text style={styles.temp}>27</Text>
-          <Text style={styles.description}>Sunny</Text>
-        </View>
-      </View>
+      <ScrollView 
+        pagingEnabled 
+        horizontal
+        showsHorizontalScrollIndicator={false} 
+        contentContainerStyle={styles.weather}>
+        {/* {days.length === 0 ? "" : <View style={styles.day}></View>} */}
+      </ScrollView>
     </View>
   );
 }
@@ -33,10 +63,10 @@ const styles = StyleSheet.create({
     fontWeight: "500", 
   },
   weather: {
-    flex : 3,
+    // flex : 3,
   },
   day: {
-    flex: 1,
+    width: SCREEN_WIDTH,
     alignItems: "center",
   },
   temp: {

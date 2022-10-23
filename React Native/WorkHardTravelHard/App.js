90c21fbd973c8ae78ee8aed79182444bdeada382
helen121027 <helen121027@gmail.com>
2022-10-23 23:43:23 +0900

    221023 노마드코더 RN WHTH persist하는 중

--- React Native/WorkHardTravelHard/App.js
+++ React Native/WorkHardTravelHard/App.js
@@ -1,14 +1,27 @@
 import { StatusBar } from 'expo-status-bar';
 import { useState } from 'react';
-import { StyleSheet, Text, View, TouchableOpacity, TextInput } from 'react-native';
+import { StyleSheet, Text, View, TouchableOpacity, TextInput, ScrollView } from 'react-native';
 import { theme } from './color';
 
 export default function App() {
   const [working, setWorking] = useState(true);
   const [text, setText] = useState("");
+  const [toDos, setToDos] = useState({});
   const travel = () => setWorking(false);
   const work = () => setWorking(true);
-  const onChangeText = (payload) => setText(event)
+  const onChangeText = (payload) => setText(payload)
+  const addToDo = () => {
+    if(text === "") {
+      return;
+    }
+    const newToDos = Object.assign(
+      {},
+      toDos,
+      {[Date.now()]: {text, work: working}},
+      )
+    setToDos(newToDos);
+    setText("");
+  }
 
   return (
     <View style={styles.container}>
@@ -22,11 +35,22 @@ export default function App() {
         </TouchableOpacity>
       </View>
       <TextInput
+        onSubmitEditing={addToDo}
         onChangeText={onChangeText}
+        returnKeyType="done"
         value={text}
         placeholder={working ? "Add a To Do": "Where do you want to go?"} 
         style={styles.input}
       />
+      <ScrollView>
+        {Object.keys(toDos).map((key) => 
+          toDos[key].working === working ? (
+            <View style = {styles.toDo} key={key}>
+              <Text style = {styles.toDoText}>{toDos[key].text}</Text>
+            </View>
+          ) : null
+        )}
+      </ScrollView>
     </View>
   );
 }
@@ -51,7 +75,19 @@ const styles = StyleSheet.create({
     paddingVertical: 15,
     paddingHorizontal: 20,
     borderRadius: 30,
-    marginTop: 20,
+    marginVertical: 20,
     fontSize: 18,
+  },
+  toDo:{
+    backgroundColor: theme.toDoBg,
+    marginBottom: 10,
+    paddingVertical: 20,
+    paddingHorizontal: 20,
+    borderRadius: 15,
+  },
+  toDoText: {
+    color: "white",
+    fontSize: 16,
+    fontWeight: "500",
   }
 });
